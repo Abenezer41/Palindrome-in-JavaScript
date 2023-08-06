5c4ddf74ccb21ee289cbda5d025058acbbd92270
abenezer <abenezertenkir41@gmail.com>
2023-08-06 11:02:01 -0700

    new update

--- js/gen.js
+++ js/gen.js
@@ -1,27 +1,27 @@
-let button = document.getElementById("gen1");
-let display = document.getElementById("d");
-let button2 = document.getElementById("btn2")
 
-button.addEventListener("click", () => {
-  let palis = [
-    "civic","radar","level","rotor","kayak","madam","refer","Abba","Aibohphobia","Bib","Bob",
-    "Civic","Deified","Detartrated","Dewed","Eve","Hannah","Kayak","Level","Madam","Malayalam",
-    "Minim","Mom","Murdrum","Noon","Nun","Otto","Peep","Pop","Racecar","Radar","Redder","Refer",
-    "Repaper","Rotator","Rotavator","Sagas","Sis","Solos","Stats","Tattarrattat","Tenet","Wow",
-    "Aha","Ava","Dad","Did","Eke","Ewe","Eye","Gag","Gig","Pip","Pup","Reviver","Sees","Sexes",
-    "Succus","Deed","Adda","Ada","Ama","Ana","Lil","Tit","Tot","Tut","Mum","Dud","Tat","Waw",
-    "Yay","Pap", "Did",
-  ];
+let clr = document.getElementById('clr');
+function PasswordRand(){
+    let  palindrome_random = document.getElementById("palindrome_random");
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    var  pass_num = document.getElementById("numpass").value;
+    
+    var palindromeLength = pass_num;
+    var palindrome = "";
 
-  let x = Math.floor(Math.random() * palis.length);
+    for (var i = 0; i < palindromeLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        palindrome += chars.substring(randomNumber, randomNumber +1);
+    }
 
-  display.innerText = palis[x];
-});
-
-function Clear() {
-  display.innerText = "";
+    palindrome_random.textContent = palindrome;
+    document.getElementById("palindrome_random").value = palindrome;
 }
 
-button2.addEventListener("click", () => {
-  Clear();
-});
+function clear(){
+  document.getElementById("palindrome_random").value = "";
+}
+
+clr.addEventListener('click',()=>{
+  clear()
+})
